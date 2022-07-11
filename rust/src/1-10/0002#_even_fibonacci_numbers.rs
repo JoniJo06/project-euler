b94eb08144c9be349c6031f719d67597c2e268a5
JoniJo06 <jonasjuergens6@gmail.com>
2022-07-11 09:41:14 +0200

    refactor 10#

--- rust/src/1-10/0002#_even_fibonacci_numbers.rs
+++ rust/src/1-10/0002#_even_fibonacci_numbers.rs
@@ -2,22 +2,17 @@ pub fn start() {
     println!("0002# even_fibonacci_numbers");
 
     let mut result: i32 = 0;
-    let mut numbers: Vec<i32> = Vec::new();
 
     let mut a = 1;
     let mut b = 2;
 
     while b < 4_000_000 {
         if b % 2 == 0 {
-            numbers.push(b);
+            result += b;
         }
         let temp = a + b;
         a = b;
         b = temp;
-    }
-
-    for number in numbers {
-        result += number;
     }
 
     println!("Result: {}", result);
