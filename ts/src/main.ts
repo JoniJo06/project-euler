079da4d151f85ede1ced0c6e38889c5bc70510b7
JoniJo06 <jonasjuergens6@gmail.com>
2022-07-11 10:34:14 +0200

    complete 3# problem (largest prime factor) in typescript

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -6,6 +6,7 @@ import assert, { equal } from 'assert';
 import fs from 'fs-extra';
 import multiples_of_3_or_5 from './1-10/1#_multiples_of_3_or_5';
 import even_fibonacci_numbers from './1-10/2#_even_fibonacci_numbers';
+import largest_prime_factor from './1-10/3#_largest_prime_factor';
 
 const DEBUG = false;
 const SOLVED = true;
@@ -13,7 +14,8 @@ const SOLVED = true;
 const compare = (n: number, result: string | number) => {
 	let rawInput: string = fs.readFileSync('./../results.txt', 'utf8').toString();
 	let results: (number | string)[] = rawInput.split('\n').map(Number);
-	results.unshift(0);
+  results.unshift(0);
+  if (results.length < n) return;
 	equal(result, results[n]);
 };
 
@@ -37,7 +39,11 @@ const main = () => {
 		exit(1);
 	}
 
-	let problems: Function[] = [multiples_of_3_or_5, even_fibonacci_numbers];
+  let problems: Function[] = [
+    multiples_of_3_or_5,
+    even_fibonacci_numbers,
+    largest_prime_factor,
+  ];
 
 	let result: string | number = 0;
 
