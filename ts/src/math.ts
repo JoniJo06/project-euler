4f8029767cd371baa9c1d1d7ad7fb9ec3f8165ad
JoniJo06 <jonasjuergens6@gmail.com>
2022-07-11 15:06:16 +0200

    replace isPrime function with a faster version

--- ts/src/math.ts
+++ ts/src/math.ts
@@ -1,6 +1,30 @@
 /** @format */
 
-export const isPrime = (num: number): boolean => {
-	for (let i = 2, s = Math.sqrt(num); i <= s; i++) if (num % i === 0) return false;
-	return num > 1;
-};
+export const isPrime = (k: number): boolean => {
+    if (k <= 1) {
+        return false;
+    }
+
+    if (k == 2 || k == 3) {
+        return true;
+    }
+
+    if (k % 2 == 0 || k % 3 == 0) {
+        return false;
+    }
+
+    let i = 5;
+    while (true) {
+        if (i * i > k) {
+            break;
+        }
+
+        if (k % i == 0 || k % (i + 2) == 0) {
+            return false;
+        }
+
+        i += 6;
+    }
+
+    return true
+}
